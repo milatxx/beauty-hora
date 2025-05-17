78b48ff581e5c461ee4c290693738ab52573366a
milatxx <146560367+milatxx@users.noreply.github.com>
2025-05-17 16:10:31 +0200

    Day 4: Create News model + migration and set up relations

--- app/Models/News.php
+++ app/Models/News.php
@@ -1,10 +1,25 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'published_at',
+        'image',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    // relatie naar auteur
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
